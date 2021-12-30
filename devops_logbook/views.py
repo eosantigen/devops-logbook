89abcf17e5bd0bea5a8415920d6f7a2d9559b661
eosantigen <eos.antigen@gmail.com>
2021-12-30 14:57:00 +0200

    nicely formatted timestamp

--- devops_logbook/views.py
+++ devops_logbook/views.py
@@ -19,12 +19,12 @@ def main(request):
                 copy['tags'] = '-'
                 f = form.save(commit=False)
                 f.tags = copy['tags']
-                f.time = now().__format__("%b %d, %Y - %H:%M")
+                f.time = now().__format__("%a, %b %d %Y - %H:%M")
                 form.save()
             else: 
                 f = form.save(commit=False)
                 f.tags = request.POST.getlist('tags')
-                f.time = now().__format__("%b %d, %Y - %H:%M")
+                f.time = now().__format__("%a, %b %d %Y - %H:%M")
                 form.save()
         return redirect('/')
     else:
